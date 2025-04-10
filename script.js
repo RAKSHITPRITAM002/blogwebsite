b97f5f301f243c5eef70bf261a22d00c1e454df3
RAKSHITPRITAM002 <rakshitpritam002@gmail.com>
2025-04-10 10:28:48 +0530

    good to go

---
+++ script.js
@@ -0,0 +1,9 @@
+function readMore(button) {
+    alert("This is just a demo. You can load full posts dynamically.");
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you for your message! We will get back to you soon.");
+  }
+  
